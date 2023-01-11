94c8589f832efd8d94c66172b86cfc9fb3d9fb29
ilis137 <iliyasshahapure1@gmail.com>
2023-01-11 15:26:36 +0530

    add:login servlet

---
+++ src/main/java/com/FirstServlet/FirstServlet.java
@@ -0,0 +1,25 @@
+package com.FirstServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1> hello world, this is my first servlet</h1>");
+        out.close();
+    }
+}
